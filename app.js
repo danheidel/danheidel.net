e645cb04799e34daed87a9cacae75b410fa2019b
Dan Heidel <dan.heidel@gmail.com>
2014-04-13 11:30:18 -0700

    env variable setup + user de-escalation

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 var express = require('express');
 
-var app = express();
-var port = process.argv[2];
+startup();
 
-if(typeof port === 'undefined'){
-	console.error('no port defined!');
-	process.exit();
-}
+var app = express();
 
 // all environments
 app.use(express.logger());
@@ -22,3 +18,30 @@ console.log('serving danheidel.net on port: ' + port);
 
 process.on('exit', function() {
 });
+
+function startup(){
+  console.log('starting as user: ' + process.env.USER);
+
+  user = parseInt(process.env.NODEUSERID) || parseInt(process.argv[2]);
+  if(!user){
+    console.error('no user specified, exiting');
+    process.exit();
+  }
+
+  //attempt to de-escalate user permissions
+  try {
+    process.setgid(user);
+    process.setuid(user);
+  } catch (e) {
+    console.error('problem setting user/group, exiting');
+    console.dir(e);
+    process.exit();
+  }
+  console.log('user changed to: ' + user);
+
+  port = parseInt(process.env.NODESERVERPORT) || parseInt(process.argv[3]);
+  if(!port){
+    console.error('no port defined, exiting');
+    process.exit();
+  }
+}
